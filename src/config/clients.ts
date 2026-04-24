f9bbc85003d579e124ed533240b215f489256943
Alessandro Barbosa dos Santos <alessandro.santos@COMETA.INTRANET>
2026-04-23 21:45:49 -0300

    feat: adicionar clientes Casa do Frango e Ultra Rota

--- src/config/clients.ts
+++ src/config/clients.ts
@@ -12,13 +12,16 @@ export const clients: ClientConfig[] = [
     apiPassword: "Serrasulmorangos@2026",
     databaseUrl: "postgres://postgres:j9pwyElD4fOaz9owOPRjchHqvluJiedyyoB1EvkOwkh4bygVAVIQ34P6INYMk9Kh@xok4kgoows80ok408gcw0coo:5432/postgres"
   },
-  /* 
-  Adicione os próximos clientes aqui seguindo o mesmo padrão:
   {
-    name: "Nome do Cliente",
-    apiEmail: "email@exemplo.com",
-    apiPassword: "senha",
-    databaseUrl: "postgres://usuario:senha@host:porta/banco"
+    name: "Casa do Frango",
+    apiEmail: "financeiro@casadofrango.com.br",
+    apiPassword: "@|1]Py6&9}£C",
+    databaseUrl: "postgres://postgres:Hldvx1Do8m5L66eqVjOVOlrKgagfcvptmspyptALJhRGowHjQ4KBxYEneoT2qqTI@k0cg88kscsw8ko804884o04o:5432/postgres"
   },
-  */
+  {
+    name: "Ultra Rota",
+    apiEmail: "victor@ultrarota.com.br",
+    apiPassword: "Cometa@ultrarota",
+    databaseUrl: "postgres://postgres:iaXZPUnlRGIi9soiV2SS8qgJLuzgcTBkqcaeVotvdeyPkOfOIntkRo0kM118SqQG@lwws8kg4ckggwgws0gg4400k:5432/postgres"
+  }
 ];
