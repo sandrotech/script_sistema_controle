54e23b7429df12c228c9be80409811220032f81b
Alessandro Barbosa dos Santos <alessandro.santos@COMETA.INTRANET>
2026-04-23 22:38:03 -0300

    adicao do costa frutas

--- src/config/clients.ts
+++ src/config/clients.ts
@@ -23,5 +23,17 @@ export const clients: ClientConfig[] = [
     apiEmail: "victor@ultrarota.com.br",
     apiPassword: "Cometa@ultrarota",
     databaseUrl: "postgres://postgres:iaXZPUnlRGIi9soiV2SS8qgJLuzgcTBkqcaeVotvdeyPkOfOIntkRo0kM118SqQG@lwws8kg4ckggwgws0gg4400k:5432/postgres"
+  },
+  {
+    name: "Costa frutas - limao",
+    apiEmail: "leandrochiaba@gmail.com",
+    apiPassword: "CostaFrutas@2026!",
+    databaseUrl: "postgres://postgres:4Lkhl8MSofMavxtkkMdL6hPP0Zvu7pydJrRm0xN2xvEp1W2Olmti66Xce6JRS4PK@igs4wokcosk8skogw884koks:5432/postgres"
+  },
+  {
+    name: "Costa frutas - maracuja",
+    apiEmail: "andredomaracuja@gmail.com",
+    apiPassword: "Maracuja@andre2026@",
+    databaseUrl: "postgres://postgres:9W50H3L55QbFaRrRLgHPmUuAXeMYiddYWEp0uDQWDBhE6bKbxQZqwZZxBpvsYy7i@co4c40cssgw4o4o0o8ksgoks:5432/postgres"
   }
 ];
